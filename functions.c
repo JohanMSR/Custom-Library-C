5ad6f34f99a9460ce1b3438355c46a2b055768a1
Johan Salazar <johanmsalazarr@gmail.com>
2024-06-30 16:24:04 -0400

    adding functions printfCharacters fast

--- functions.c
+++ functions.c
@@ -6,17 +6,24 @@ int sum(int numb){
     return numb;
 }
 
-int remainder(int numb){
+int remain(int numb){
     numb--;
     return numb;
 }
 
-char printCharacters(char name[], int amount){
+char generateCharacters(char name[], char character[], int amount){
     for (int i = 0; i <= amount; i++){
-        strcat(name, "#");
+        strcat(name, character);
     }
     return name[100];
 }
+
+void printCharacters(char character[], int amount){
+    for (int i = 0; i <= amount; i++){
+        printf("%s", character);
+    }
+}
+
 
 
 
