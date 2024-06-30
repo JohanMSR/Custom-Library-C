0816bf734afc0c0855fc693596e26eda32ca971e
Johan Salazar <johanmsalazarr@gmail.com>
2024-06-30 15:36:15 -0400

    update 30/06/24, 2 new functions

--- functions.c
+++ functions.c
@@ -1,4 +1,23 @@
-int sum(int x){
-    x++;
-    return x;
+/*Basic Functions*/
+#include <string.h>
+
+int sum(int numb){
+    numb++;
+    return numb;
 }
+
+int remainder(int numb){
+    numb--;
+    return numb;
+}
+
+char printCharacters(char name[], int amount){
+    for (int i = 0; i <= amount; i++){
+        strcat(name, "#");
+    }
+    return name[100];
+}
+
+
+
+
